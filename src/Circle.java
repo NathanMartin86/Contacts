ccdeac7a94a17ac55ee1ddbf7c867f12d658cb65
macbookair <nj.martin123@gmail.com>
2015-10-13 09:37:33 -0400

    Initial Import

---
+++ src/Circle.java
@@ -0,0 +1,19 @@
+/**
+ * Created by macbookair on 10/8/15.
+ */
+public class Circle {
+    public int radius;
+    public String color;
+
+    public void setRadius (int r ){
+        this.radius = r;
+    }
+
+    public int getRadius (){
+        return radius;
+
+    }
+
+
+
+}
---
+++ src/Email1.java
@@ -0,0 +1,16 @@
+/**
+ * Created by macbookair on 10/8/15.
+ */
+public class Email1 {
+    public String subject;
+    public String Body;
+    public String destination;
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+}
