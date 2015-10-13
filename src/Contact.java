2b9a97f0d416e545cbd04722f385a8cecc93aaf8
macbookair <nj.martin123@gmail.com>
2015-10-13 10:21:25 -0400

    Test Code to practice constructors and data structures

---
+++ src/Contact.java
@@ -0,0 +1,17 @@
+/**
+ * Created by macbookair on 10/8/15.
+ */
+public class Contact {
+    public String name;
+    public String address;
+    public String email;
+    public int age;
+
+    public Contact(String name, int age){
+        this.name = name;
+        this.age = age;
+
+    }
+
+}
+
